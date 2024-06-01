69c7562c0b0ffa412ee66e8a7757c2739960d923
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2024-05-31 22:06:27 -0700

    Add files via upload

---
+++ home-manager/dotfiles/waybar/default.nix
@@ -0,0 +1,310 @@
+{ config, lib, pkgs, ... }:
+
+{
+    programs.waybar = {
+      enable = true;
+      systemd = {
+        enable = false;
+        target = "graphical-session.target";
+      };
+      style = ''
+               * {
+                 font-family: "JetBrainsMono Nerd Font";
+                 font-size: 10pt;
+                 font-weight: bold;
+                 border-radius: 8px;
+                 transition-property: background-color;
+                 transition-duration: 0.5s;
+               }
+               @keyframes blink_red {
+                 to {
+                   background-color: rgb(242, 143, 173);
+                   color: rgb(26, 24, 38);
+                 }
+               }
+               .warning, .critical, .urgent {
+                 animation-name: blink_red;
+                 animation-duration: 1s;
+                 animation-timing-function: linear;
+                 animation-iteration-count: infinite;
+                 animation-direction: alternate;
+               }
+               window#waybar {
+                 background-color: transparent;
+               }
+               window > box {
+                 margin-left: 5px;
+                 margin-right: 5px;
+                 margin-top: 5px;
+                 background-color: #1e1e2a;
+                 padding: 3px;
+                 padding-left:8px;
+                 border: 2px none #33ccff;
+               }
+         #workspaces {
+                 padding-left: 0px;
+                 padding-right: 4px;
+               }
+         #workspaces button {
+                 padding-top: 5px;
+                 padding-bottom: 5px;
+                 padding-left: 6px;
+                 padding-right: 6px;
+               }
+         #workspaces button.active {
+                 background-color: rgb(181, 232, 224);
+                 color: rgb(26, 24, 38);
+               }
+         #workspaces button.urgent {
+                 color: rgb(26, 24, 38);
+               }
+         #workspaces button:hover {
+                 background-color: rgb(248, 189, 150);
+                 color: rgb(26, 24, 38);
+               }
+               tooltip {
+                 background: rgb(48, 45, 65);
+               }
+               tooltip label {
+                 color: rgb(217, 224, 238);
+               }
+         #custom-launcher {
+                 font-size: 20px;
+                 padding-left: 4px;
+                 padding-right: 0px;
+                 color: #7ebae4;
+               }
+         #mode, #clock, #memory, #temperature,#cpu,#mpd, #custom-wall, #temperature, #backlight, #pulseaudio, #network, #battery, #custom-powermenu, #cava {
+                 padding-left: 5px;
+                 padding-right: 5px;
+               }
+               /* #mode { */
+               /* 	margin-left: 10px; */
+               /* 	background-color: rgb(248, 189, 150); */
+               /*     color: rgb(26, 24, 38); */
+               /* } */
+
+        #battery.critical:not(.charging) {
+          background-color: @red;
+          color: @theme_text_color;
+          animation-name: blink;
+          animation-duration: 0.5s;
+          animation-timing-function: linear;
+          animation-iteration-count: infinite;
+          animation-direction: alternate;
+          box-shadow: inset 0 -3px transparent;
+        }
+         #memory {
+                 color: rgb(181, 232, 224);
+               }
+         #cpu {
+                 color: rgb(245, 194, 231);
+               }
+         #clock {
+                 color: rgb(217, 224, 238);
+               }
+        /* #idle_inhibitor {
+                 color: rgb(221, 182, 242);
+               }*/
+         #custom-wall {
+                 color: #33ccff;
+            }
+         #temperature {
+                 color: rgb(150, 205, 251);
+               }
+         #temperature.critical {
+                background-color: @red;
+		padding-right: 2px;
+		padding-left: 2px;
+         }
+         #backlight {
+                 color: rgb(248, 189, 150);
+               }
+         #pulseaudio {
+                 color: rgb(245, 224, 220);
+               }
+         #network {
+                 color: #ABE9B3;
+               }
+         #network.disabled {
+           background-color: @surface0;
+           color: @text;
+         }
+         #network.disconnected {
+                 color: rgb(255, 255, 255);
+               }
+         #custom-powermenu {
+                 color: rgb(242, 143, 173);
+                 padding-right: 8px;
+               }
+         #tray {
+                 padding-right: 8px;
+                 padding-left: 10px;
+               }
+         #mpd.paused {
+                 color: #414868;
+                 font-style: italic;
+               }
+         #mpd.stopped {
+                 background: transparent;
+               }
+         #mpd {
+                 color: #c0caf5;
+               }
+         #cava{
+                 font-family: "JetBrainsMono Nerd Font";
+		 font-size: 15px;
+                 color: #33ccff;
+		 padding-left: 2px;
+               }
+      '';
+      settings = [{
+        "layer" = "top";
+        "position" = "top";
+        modules-left = [
+          "custom/launcher"
+          "mpd"
+          "cava"
+        ];
+        modules-center = [
+          "clock"
+        ];
+        modules-right = [
+          "pulseaudio"
+          "backlight"
+	  "cpu"
+          "memory"
+	  "temperature"
+          "network"
+	  "battery"
+          "custom/powermenu"
+          "tray"
+        ];
+
+	"backlight" = {
+	   "format" = "{icon} {percent}%"; 
+	   "format-icons" = ["󰃠"];
+	   "interval" = 1;
+ 	};
+        "battery" = {
+            "states" = {
+              "good" = 95;
+              "warning" = 30;
+              "critical" = 20;
+            };
+            "format" = "{icon} {capacity}%";
+            "format-charging" = " {capacity}%";
+            "format-plugged" = " {capacity}%";
+            "format-alt" = "{time} {icon}";
+            "format-icons" = ["󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
+        };
+        "cava" = {
+            "hide_on_silence" = false;
+            "framerate" = 30;
+            "bars" = 10;
+            "format-icons" = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
+            "input_delay" = 1;
+            # "noise_reduction" = 0.77;
+            "sleep_timer" = 5;
+            "bar_delimiter" = 0;
+        };
+        "custom/launcher" = {
+          "format" = " ";
+          "on-click" = "pkill rofi || rofi2";
+          "on-click-middle" = "exec default_wall";
+          "on-click-right" = "exec wallpaper_random";
+          "tooltip" = false;
+        };
+        "temperature" = {
+          # "thermal-zone" = 2;
+          "hwmon-path" = "/sys/class/hwmon/hwmon2/temp1_input";
+          "critical-threshold" = 80;
+          # "format-critical" = "{temperatureC}°C {icon}";
+          "format" = "{icon} {temperatureC}°C";
+          "format-icons" = ["" "" ""];
+          "interval" = 2;
+        };
+        "cava" = {
+          "exec" = "sleep 1s && cava";
+          "tooltip" = false;
+        };
+        "pulseaudio" = {
+          "scroll-step" = 1;
+          "format" = "{icon} {volume}%";
+          "format-muted" = "󰖁 Muted";
+          "format-icons" = {
+            "default" = [ "" "" "" ];
+          };
+          "on-click" = "pavucontrol";
+          "tooltip" = false;
+        };
+        "clock" = {
+	  "format" = "{:%I:%M %p}";
+          "format-alt" = "{:%R 󰃭 %m·%d·%y}";
+          "tooltip-format" = "<tt>{calendar}</tt>";
+          "calendar" = {
+            "mode" = "month";
+            "mode-mon-col" = 3;
+            "on-scroll" = 1;
+            "on-click-right" = "mode";
+            "format" = {
+              "months" = "<span color='#ffead3'><b>{}</b></span>";
+              "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
+              "today" = "<span color='#ff6699'><b>{}</b></span>";
+            };
+          };
+          "actions" = {
+            "on-click-right" = "mode";
+            "on-click-forward" = "tz_up";
+            "on-click-backward" = "tz_down";
+            "on-scroll-up" = "shift_up";
+            "on-scroll-down" = "shift_down";
+          };
+        };
+        "memory" = {
+          "interval" = 1;
+          "format" = "󰍛 {percentage}%";
+          "states" = {
+            "warning" = 85;
+          };
+        };
+        "cpu" = {
+          "interval" = 1;
+          "format" = "󰘚 {usage}%";
+        };
+        "mpd" = {
+          "max-length" = 25;
+          "format" = "<span foreground='#bb9af7'></span> {title}";
+          "format-paused" = " {title}";
+          "format-stopped" = "<span foreground='#bb9af7'></span>";
+          "format-disconnected" = "";
+          "on-click" = "mpc --quiet toggle";
+          "on-click-right" = "mpc update; mpc ls | mpc add";
+          "on-click-middle" = "kitty --class='ncmpcpp' ncmpcpp ";
+          "on-scroll-up" = "mpc --quiet prev";
+          "on-scroll-down" = "mpc --quiet next";
+          "smooth-scrolling-threshold" = 5;
+          "tooltip-format" = "{title} - {artist} ({elapsedTime:%M:%S}/{totalTime:%H:%M:%S})";
+        };
+        "network" = {
+          # "interface" = "wlp2*"; # (Optional) To force the use of this interface
+          "format-wifi" = "󰤨 {signalStrength}%";
+          "format-ethernet" = "󱘖 Wired";
+          "tooltip-format" = "󱘖 {ipaddr}  {bandwidthUpBytes}  {bandwidthDownBytes}";
+          "format-linked" = "󱘖 {ifname} (No IP)";
+          "format-disconnected" = " Disconnected";
+          "format-alt" = "󰤨 {essid}";
+          "interval" = 5;
+        };
+        "custom/powermenu" = {
+          "format" = "";
+          "on-click" = "pkill rofi || ~/.config/rofi/powermenu/type-3/powermenu.sh";
+          "tooltip" = false;
+        };
+        "tray" = {
+          "icon-size" = 15;
+          "spacing" = 5;
+        };
+      }];
+    };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,436 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./dotfiles
+  ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "kended";
+  home.homeDirectory = "/home/kended";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # unfree pkgs
+  nixpkgs.config.allowUnfree = true;
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    #generic
+    neovim
+    firefox
+    ( nerdfonts.override { fonts = [ "JetBrainsMono" ]; } )
+
+    #utils
+    neofetch
+    htop
+    swww
+    rofi-wayland
+    pavucontrol
+    libnotify
+    ffmpeg
+    grim
+    mpv
+
+    #misc
+    discord
+    steam
+  ];
+
+  home.file = {
+  };
+
+  programs.git = {
+    enable = true;
+    userEmail = "kclamberth@yahoo.com";
+    userName = "Kclamberth";
+  };
+
+  programs.kitty = {
+    enable = true;
+
+    theme = "Catppuccin-Mocha";
+    
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 10;
+    };
+
+    settings = {
+      confirm_os_window_close = 0;
+      background_opacity = "0.8";  
+      window_padding_width = 5;
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      mouse_hide_wait = 60;
+      
+      ## Tabs
+      tab_title_template = "{index}";
+      active_tab_font_style = "normal";
+      inactive_tab_font_style = "normal";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "round";
+      active_tab_foreground = "#1e1e2e";
+      active_tab_background = "#cba6f7";
+      inactive_tab_foreground = "#bac2de";
+      inactive_tab_background = "#313244";
+    };
+
+    keybindings = {  
+      ## Tabs
+      "alt+1" = "goto_tab 1";
+      "alt+2" = "goto_tab 2";
+      "alt+3" = "goto_tab 3";
+      "alt+4" = "goto_tab 4";
+
+      ## Unbind
+      "ctrl+shift+left" = "no_op";
+      "ctrl+shift+right" = "no_op";
+
+    };
+  };
+
+  dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+  };
+
+  home.pointerCursor = {
+      gtk.enable = true;
+      x11.enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 24;
+  };
+
+  qt = {
+      enable = true;
+      platformTheme.name = "gtk"; # gnome
+  };
+
+  gtk = {
+      enable = true;
+      theme = {
+        name = "Catppuccin-Macchiato-Compact-Mauve-Dark";
+        package = pkgs.catppuccin-gtk.override {
+          accents = ["mauve"];
+          size = "compact";
+          #tweaks = [ "rimless" "black" ];
+          variant = "macchiato";
+        };
+  };
+
+  iconTheme = {
+        package = pkgs.gnome.adwaita-icon-theme;
+        name = "Adwaita";
+        #name = "Yaru-magenta-dark";
+        #package = pkgs.yaru-theme;
+  };
+
+  gtk3.extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+  };
+
+  gtk4.extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+  };
+
+  #font = {
+      #  name = "Sans";
+      #  size = 11;
+      #};
+  };
+
+  programs.mpv = {
+     enable = true;
+     config = {
+	panscan="1.0";
+	hwdec="vaapi";
+	profile="gpu-hq";
+	scale="ewa_lanczossharp";
+	cscale="ewa_lanczossharp";
+	deband="yes";
+     
+	sub-font="JetBrains Mono";
+	osd-font="JetBrains Mono";
+     };
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+
+      exec-once = [
+        "swww init"
+        "swww img ~/Wallpapers/wallpaper.png"
+        "waybar"
+        "dunst"
+      ];
+      monitor = "..., preferred, auto, auto";
+      #env = [ "XCURSOR_SIZE, 24" ];
+
+      input = {
+        kb_layout = "us";
+        # kb_variant =
+        # kb_model =
+        # kb_options = "grp:win_space_toggle";
+        # kb_rules =
+
+        follow_mouse = 1;
+
+        touchpad = { natural_scroll = "yes"; };
+
+        sensitivity = 0;
+      };
+
+      general = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        gaps_in = 5;
+        gaps_out = [ 5 5 5 5 ];
+        border_size = 1;
+        "col.active_border" = "rgba(ca9ee6ff) rgba(f2d5cfff) 45deg";
+        "col.inactive_border" = "rgba(b4befecc) rgba(6c7086cc) 45deg";
+
+        layout = "dwindle";
+
+        # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+        allow_tearing = false;
+      };
+
+      decoration = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+        rounding = 10;
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+        };
+
+        drop_shadow = "yes";
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+      };
+
+      animations = {
+        enabled = "yes";
+
+        # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      dwindle = {
+        # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+        # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        pseudotile = "yes";
+        # you probably want this
+        preserve_split = "yes";
+      };
+
+      master = {
+        # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+        new_is_master = true;
+      };
+      gestures = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        workspace_swipe = "off";
+      };
+
+      misc = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        force_default_wallpaper =
+          -1; # Set to 0 to disable the anime mascot wallpapers
+      };
+      # Example per-device config
+      # See https://wiki.hyprland.org/Configuring/Keywords/#executing for more
+      #"device:epic-mouse-v1" = { sensitivity = -0.5; };
+
+      # Example windowrule v1
+      # windowrule = float, ^(kitty)$
+      # Example windowrule v2
+      # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
+      # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
+      # windowrule = [
+      #  "float,polkit-kde-authentication-agent-1"
+      #  "center,polkit-kde-authentication-agent-1"
+      #  "size 500 500,polkit-kde-authentication-agent-1"
+      #];
+
+      # See https://wiki.hyprland.org/Configuring/Keywords/ for more
+      "$mainMod" = "SUPER";
+      bind = [
+        "$mainMod, Q, exec, kitty"
+        # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
+        "$mainMod, C, killactive,"
+        "$mainMod, M, exit,"
+        "$mainMod, V, togglefloating,"
+        "$mainMod, S, exec, rofi -show drun --show-icons"
+        "$mainMod, P, pseudo,"
+	"$mainMod, J, togglesplit,"
+
+        # Move focus with mainMod + arrow keys
+        "$mainMod, H, movefocus, l"
+        "$mainMod, L, movefocus, r"
+        "$mainMod, K, movefocus, u"
+        "$mainMod, J, movefocus, d"
+
+        # Switch workspaces with mainMod + [0-9]
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        # Move active window to a workspace with mainMod + SHIFT + [0-9]
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+        # ? I don't need it (I think)
+        # Example special workspace (scratchpad)
+        # "$mainMod, S, togglespecialworkspace, magic"
+        # "$mainMod SHIFT, S, movetoworkspace, special:magic"
+
+        # Scroll through existing workspaces with mainMod + scroll
+        "$mainMod, right, workspace, e+1"
+        "$mainMod, left, workspace, e-1"
+
+        "$mainMod SHIFT, right, movetoworkspace, +1"
+        "$mainMod SHIFT, left, movetoworkspace, -1"
+
+        # Move Windows
+        "$mainMod SHIFT, H, movewindow, l"
+        "$mainMod SHIFT, L, movewindow, r"
+        "$mainMod SHIFT, K, movewindow, u"
+        "$mainMod SHIFT, J, movewindow, d"
+
+        # Resize window
+        "$mainMod CTRL, l, resizeactive, 50 0"
+        "$mainMod CTRL, h, resizeactive, -50 0"
+        "$mainMod CTRL, k, resizeactive, 0 -50"
+        "$mainMod CTRL, j, resizeactive, 0 50"
+
+      ];
+
+      bindm = [
+        # Move/resize windows with mainMod + LMB/RMB and dragging
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+    };
+  };
+
+  services.dunst = {
+    enable = true;
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    settings = {
+      global = {
+        rounded = "yes";
+        origin = "top-right";
+        monitor = "0";
+        alignment = "left";
+        vertical_alignment = "center";
+        width = "400";
+        height = "400";
+        scale = 0;
+        gap_size = 0;
+        progress_bar = true;
+        transparency = 0;
+        text_icon_padding = 0;
+        separator_color = "frame";
+        sort = "yes";
+        idle_threshold = 120;
+        line_height = 0;
+        markup = "full";
+        show_age_threshold = 60;
+        ellipsize = "middle";
+        ignore_newline = "no";
+        stack_duplicates = true;
+        sticky_history = "yes";
+        history_length = 20;
+        always_run_script = true;
+        corner_radius = 10;
+        follow = "mouse";
+        font = "Source Sans Pro 10";
+        format = "<b>%s</b>\\n%b"; #format = "<span foreground='#f3f4f5'><b>%s %p</b></span>\n%b"
+        frame_color = "#232323";
+        frame_width = 1;
+        offset = "15x15";
+        horizontal_padding = 10;
+        icon_position = "left";
+        indicate_hidden = "yes";
+        min_icon_size = 0;
+        max_icon_size = 64;
+        mouse_left_click = "do_action, close_current";
+        mouse_middle_click = "close_current";
+        mouse_right_click = "close_all";
+        padding = 10;
+        plain_text = "no";
+        separator_height = 2;
+        show_indicators = "yes";
+        shrink = "no";
+        word_wrap = "yes";
+        browser = "/usr/bin/env librewolf -new-tab";
+      };
+
+      fullscreen_delay_everything = {fullscreen = "delay";};
+
+      urgency_critical = {
+        background = "#d64e4e";
+        foreground = "#f0e0e0";
+      };
+      urgency_low = {
+        background = "#232323";
+        foreground = "#2596be";
+      };
+      urgency_normal = {
+        background = "#1e1e2a";
+        foreground = "#2596be";
+      };
+    };
+  };
+
+  home.sessionVariables = {
+     EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
