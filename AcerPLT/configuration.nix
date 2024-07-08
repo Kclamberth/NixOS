aac27b9df34482267ff13451ac6869b002b2bee6
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2024-07-07 17:00:01 -0700

    Add files via upload

--- AcerPLT/configuration.nix
+++ AcerPLT/configuration.nix
@@ -92,17 +92,15 @@
   # VPN
   services.tailscale.enable = true;
 
-  # Sound (Pipewire)
-  # Remove sound.enable or set it to false if you had it set previously, as sound.enable is only meant for ALSA-based configurations
-  # rtkit is optional but recommended
+  # Sound (pipewire)
+  #sound.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+  # jack.enable = true;
   };
 
   xdg.portal.enable = true;
